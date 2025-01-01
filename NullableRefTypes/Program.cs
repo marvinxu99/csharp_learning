07ae53b9c924cf67a2fc1f6876e1f305c3e229b5
Marvin Xu <marvinxu99@hotmail.com>
2025-01-01 10:20:17 -0800

    ...

---
+++ NullableRefTypes/Program.cs
@@ -0,0 +1,12 @@
+﻿// Express your design intent more clearly with nullable and non-nullable reference types
+// https://learn.microsoft.com/en-us/dotnet/csharp/tutorials/nullable-reference-types
+
+namespace NullableRefTypes;
+
+internal class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
