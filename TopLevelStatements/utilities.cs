9ed9ed1ea719aea2c3e0a99468c816b933608b73
Marvin Xu <marvinxu99@hotmail.com>
2024-12-31 19:12:42 -0800

    ...

---
+++ TopLevelStatements/utilities.cs
@@ -0,0 +1,39 @@
+﻿namespace TopLevelStatements;
+
+public static class Utilities
+{
+    public static async Task ShowConsoleAnimation()
+    {
+        string[] animations = ["| -", "/ \\", "- |", "\\ /"];
+        for (int i = 0; i < 20; i++)
+        {
+            foreach (string s in animations)
+            {
+                Console.Write(s);
+                await Task.Delay(50);
+                Console.Write("\b\b\b");
+            }
+        }
+        Console.WriteLine();
+    }
+
+    public static async Task ShowConsoleAnimationPRE()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.Write("| -");
+            await Task.Delay(50);
+            Console.Write("\b\b\b");
+            Console.Write("/ \\");
+            await Task.Delay(50);
+            Console.Write("\b\b\b");
+            Console.Write("- |");
+            await Task.Delay(50);
+            Console.Write("\b\b\b");
+            Console.Write("\\ /");
+            await Task.Delay(50);
+            Console.Write("\b\b\b");
+        }
+        Console.WriteLine();
+    }
+}
