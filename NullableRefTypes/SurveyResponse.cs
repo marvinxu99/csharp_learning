c4d271ede03899e60cf78119793fe0f55bbb7600
Marvin Xu <marvinxu99@hotmail.com>
2025-01-01 17:21:25 -0800

    ...

---
+++ NullableRefTypes/SurveyResponse.cs
@@ -0,0 +1,74 @@
+﻿namespace NullableRefTypes;
+
+public class SurveyResponse(int id)
+{
+    public int Id { get; } = id;
+    private Dictionary<int, string>? surveyResponses;
+
+    private static readonly Random randomGenerator = new();
+
+    public static SurveyResponse GetRandomId() => new(randomGenerator.Next());
+
+
+    public bool AnswerSurvey(IEnumerable<SurveyQuestion> questions)
+    {
+        if (ConsentToSurvey())
+        {
+            surveyResponses = new Dictionary<int, string>();
+
+            int index = 0;
+            foreach (var question in questions)
+            {
+                var answer = GenerateAnswer(question);
+                if (answer != null)
+                {
+                    surveyResponses.Add(index, answer);
+                }
+                index++;
+            }
+        }
+        return surveyResponses != null;
+    }
+
+    private static bool ConsentToSurvey() => randomGenerator.Next(0, 2) == 1;
+
+    private static string? GenerateAnswer(SurveyQuestion question)
+    {
+        switch (question.TypeOfQuestion)
+        {
+            case QuestionType.YesNo:
+                int n = randomGenerator.Next(-1, 2);
+                return (n == -1) ? default : (n == 0) ? "No" : "Yes";
+
+            case QuestionType.Number:
+                n = randomGenerator.Next(-30, 101);
+                return (n < 0) ? default : n.ToString();
+
+            case QuestionType.Text:
+            default:
+                //switch (randomGenerator.Next(0, 5))
+                //{
+                //    case 0:
+                //        return default;
+                //    case 1:
+                //        return "Red";
+                //    case 2:
+                //        return "Green";
+                //    case 3:
+                //        return "Blue";
+                //}
+                //return "Red. No, Green. Wait.. Blue... AAARGGGGGHHH!";
+                return randomGenerator.Next(0, 5) switch
+                {
+                    0 => default,
+                    1 => "Red",
+                    2 => "Green",
+                    3 => "Blue",
+                    _ => "Red. No, Green. Wait.. Blue... AAARGGGGGHHH!"
+                };
+        }
+    }
+
+    public bool AnsweredSurvey => surveyResponses != null;
+    public string Answer(int index) => surveyResponses?.GetValueOrDefault(index) ?? "No answer";
+}
---
+++ NullableRefTypes/SurveyRun.cs
@@ -0,0 +1,30 @@
+﻿namespace NullableRefTypes;
+
+public class SurveyRun
+{
+    private readonly List<SurveyQuestion> surveyQuestions = [];
+
+    private List<SurveyResponse>? respondents;
+
+    public void AddQuestion(QuestionType type, string question) =>
+        AddQuestion(new SurveyQuestion(type, question));
+
+    public void AddQuestion(SurveyQuestion surveyQuestion) => surveyQuestions.Add(surveyQuestion);
+
+    public void PerformSurvey(int numberOfRespondents)
+    {
+        int respondentsConsenting = 0;
+        respondents = [];
+        while (respondentsConsenting < numberOfRespondents)
+        {
+            var respondent = SurveyResponse.GetRandomId();
+            if (respondent.AnswerSurvey(surveyQuestions))
+                respondentsConsenting++;
+            respondents.Add(respondent);
+        }
+    }
+
+    public IEnumerable<SurveyResponse> AllParticipants => (respondents ?? Enumerable.Empty<SurveyResponse>());
+    public ICollection<SurveyQuestion> Questions => surveyQuestions;
+    public SurveyQuestion GetQuestion(int index) => surveyQuestions[index];
+}
