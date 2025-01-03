460e913921c0bb70334f0e3729b8739b1751337d
Marvin Xu <marvinxu99@hotmail.com>
2025-01-02 19:45:07 -0800

    ...

---
+++ WebAPIClient/Program.cs
@@ -0,0 +1,45 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/csharp/tutorials/console-webapiclient
+
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace WebAPIClient;
+
+internal class Program
+{
+    static async Task Main()
+    {
+        using HttpClient client = new();
+
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+        client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+        var repositories = await ProcessRepositoriesAsync(client);
+
+        foreach (var repo in repositories)
+        {
+            Console.WriteLine($"Name: {repo.Name}");
+            Console.WriteLine($"HomePage: {repo.Homepage}");
+            Console.WriteLine($"GutHub: {repo.GitHubHomeUrl}");
+            Console.WriteLine($"Description: {repo.Description}");
+            Console.WriteLine($"Watchers: {repo.Watchers:#,0}");
+            Console.WriteLine($"Last push: {repo.LastPush}");
+            Console.WriteLine();
+        }
+
+    }
+    private static async Task<List<Repository>> ProcessRepositoriesAsync(HttpClient client)
+    {
+        // Return string
+        //var json = await client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+        await using Stream stream =
+            await client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
+        var repositories =
+            await JsonSerializer.DeserializeAsync<List<Repository>>(stream);
+
+        return repositories ?? [];
+    }
+}
---
+++ WebAPIClient/Repository.cs
@@ -0,0 +1,20 @@
+﻿namespace WebAPIClient;
+
+//public record class Repository(string name);
+
+using System.Text.Json.Serialization;
+
+// Changes the name of the name property to Name.
+// Adds the JsonPropertyNameAttribute to specify how this property appears in the JSON.
+public record class Repository(
+    [property: JsonPropertyName("name")] string Name,
+    [property: JsonPropertyName("description")] string Description,
+    [property: JsonPropertyName("html_url")] Uri GitHubHomeUrl,
+    [property: JsonPropertyName("homepage")] Uri Homepage,
+    [property: JsonPropertyName("watchers")] int Watchers,
+    [property: JsonPropertyName("pushed_at")] DateTime LastPushUtc)
+{
+    public DateTime LastPush => LastPushUtc.ToLocalTime();
+}
+
+
