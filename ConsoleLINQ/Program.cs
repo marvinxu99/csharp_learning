d6e846d1c675fce8ff6cea5bc1b0c9e88879a399
Marvin Xu <marvinxu99@hotmail.com>
2025-01-02 21:06:56 -0800

    ...

--- ConsoleLINQ/Program.cs
+++ ConsoleLINQ/Program.cs
@@ -5,9 +5,11 @@ internal class Program
     static void Main()
     {
         //var startingDeck2 = Suits().SelectMany(suit => Ranks().Select(rank => new { Suit = suit, Rank = rank }));
-        var startingDeck = from s in Suits()
-                           from r in Ranks()
-                           select new { Suit = s, Rank = r };
+        var startingDeck = (from s in Suits()
+                            from r in Ranks()
+                            select new { Suit = s, Rank = r })
+                            .LogQuery("Starting Deck")
+                            .ToArray();
 
 
         // Display each card that we've generated and placed in startingDeck in the console
@@ -15,6 +17,51 @@ internal class Program
         {
             Console.WriteLine(card);
         }
+
+        Console.WriteLine();
+
+        // Shuffling using InterleaveSequenceWith<T>();
+        // 52 cards in a deck, so 52 / 2 = 26
+        var top = startingDeck.Take(26);
+        var bottom = startingDeck.Skip(26);
+        var shuffle = top.InterleaveSequenceWith(bottom);
+        foreach (var c in shuffle)
+        {
+            Console.WriteLine(c);
+        }
+
+        Console.WriteLine();
+        var times = 0;
+        shuffle = startingDeck;
+        do
+        {
+            // Out shuffle
+            /*
+            shuffle = shuffle.Take(26)
+                .LogQuery("Top Half")
+                .InterleaveSequenceWith(shuffle.Skip(26)
+                .LogQuery("Bottom Half"))
+                .LogQuery("Shuffle")
+                .ToArry();
+            */
+
+            // In shuffle
+            shuffle = shuffle.Skip(26).LogQuery("Bottom Half")
+                .InterleaveSequenceWith(shuffle.Take(26).LogQuery("Top Half"))
+                .LogQuery("Shuffle")
+                .ToArray();
+
+            foreach (var card in shuffle)
+            {
+                Console.WriteLine(card);
+            }
+            Console.WriteLine();
+            times++;
+
+        } while (!startingDeck.SequenceEquals(shuffle));
+
+        Console.WriteLine(times);
+
     }
 
     static IEnumerable<string> Suits()
