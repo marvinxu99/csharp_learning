c5256757acfa978e8c8dab8944cea596014d0034
Marvin Xu <marvinxu99@hotmail.com>
2025-01-04 16:04:02 -0800

    ...

---
+++ ConsoleLINQ/LINQQueryOperators.cs
@@ -0,0 +1,13 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/csharp/linq/standard-query-operators/
+
+namespace ConsoleLINQ;
+
+internal class LINQQueryOperators
+{
+    internal stataic void RunTest()
+    {
+
+    }
+
+
+}
