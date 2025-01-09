b8f65810d5eae27c9a3f4d49ff551a39095e559d
Marvin Xu <marvinxu99@hotmail.com>
2025-01-08 18:20:46 -0800

    ...

--- WebAPIClient/Repository.cs
+++ WebAPIClient/Repository.cs
@@ -7,12 +7,24 @@ using System.Text.Json.Serialization;
 // Changes the name of the name property to Name.
 // Adds the JsonPropertyNameAttribute to specify how this property appears in the JSON.
 public record class Repository(
-    [property: JsonPropertyName("name")] string Name,
-    [property: JsonPropertyName("description")] string Description,
-    [property: JsonPropertyName("html_url")] Uri GitHubHomeUrl,
-    [property: JsonPropertyName("homepage")] Uri Homepage,
-    [property: JsonPropertyName("watchers")] int Watchers,
-    [property: JsonPropertyName("pushed_at")] DateTime LastPushUtc)
+    [property: JsonPropertyName("name")]
+    string Name,
+
+    [property: JsonPropertyName("description")]
+    string Description,
+
+    [property: JsonPropertyName("html_url")]
+    Uri GitHubHomeUrl,
+
+    [property: JsonPropertyName("homepage")]
+    Uri Homepage,
+
+    [property: JsonPropertyName("watchers")]
+    int Watchers,
+
+    [property: JsonPropertyName("pushed_at")]
+    DateTime LastPushUtc
+)
 {
     public DateTime LastPush => LastPushUtc.ToLocalTime();
 }
