809bbce568cf401935927e613dda7bc8e22f7c48
Marvin Xu <marvinxu99@hotmail.com>
2025-01-11 15:18:20 -0800

    ...

---
+++ StandardQueryOperators/LetSample1.cs
@@ -0,0 +1,33 @@
+﻿namespace StandardQueryOperators;
+
+internal class LetSample1
+{
+    internal static void RunTest()
+    {
+        string[] strings =
+        [
+            "A penny saved is a penny earned.",
+            "The early bird catches the worm.",
+            "The pen is mightier than the sword."
+        ];
+
+        // Split the sentence into an array of words
+        // and select those whose first letter is a vowel.
+        var earlyBirdQuery =
+            from sentence in strings
+            let words = sentence.Split(' ')
+            from word in words
+            let w = word.ToLower()
+            where w[0] == 'a' || w[0] == 'e'      // more efficient this way
+                || w[0] == 'i' || w[0] == 'o'
+                || w[0] == 'u'
+            //where "aeiou".Contains(w[0])
+            select word;
+
+        // Execute the query.
+        foreach (var v in earlyBirdQuery)
+        {
+            Console.WriteLine("\"{0}\" starts with a vowel", v);
+        }
+    }
+}
---
+++ StandardQueryOperators/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Linq;
+namespace StandardQueryOperators;
+
+internal class Program
+{
+    static void Main()
+    {
+        //////////////////////////////////////
+        // to XML
+        var students = Sources.Students;
+        var studentsToXML = new XElement("Root",
+            from student in students
+            let scores = string.Join(",", student.Scores)
+            select new XElement("student",
+                        new XElement("First", student.FirstName),
+                        new XElement("Last", student.LastName),
+                        new XElement("Scores", scores)
+                    ) // end "student"
+                ); // end "Root"
+
+        // Execute the query.
+        Console.WriteLine(studentsToXML);
+
+        /////////////////////////////////////
+        // Let clause example
+        //LetSample1.RunTest();
+
+    }
+}
